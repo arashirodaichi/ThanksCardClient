959fbdf5901ee654697945f108de3a26c65d32e4
ishigakiarashiro <arashiro_d@occ.co.jp>
2020-06-05 13:39:26 +0900

    人事メニュー画面作成

---
+++ ThanksCardClient/Views/Departmentmenu1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ThanksCardClient.Views
+{
+    /// <summary>
+    /// Interaction logic for Departmentmenu1
+    /// </summary>
+    public partial class Departmentmenu1 : UserControl
+    {
+        public Departmentmenu1()
+        {
+            InitializeComponent();
+        }
+    }
+}
