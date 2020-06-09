1431ff0ebaa1278196850bf056e7731fa5af49ed
ishigakiarashiro <arashiro_d@occ.co.jp>
2020-06-09 11:20:20 +0900

    受け取りランキング

---
+++ ThanksCardClient/Views/Test.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace ThanksCardClient.Views
+{
+    /// <summary>
+    /// Interaction logic for Test
+    /// </summary>
+    public partial class Test : UserControl
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
